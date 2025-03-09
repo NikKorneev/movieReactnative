0512d2a3417fc1077984d9fc87d2ae57946587e1
Nick <nikita_korneev101@mail.ru>
2025-03-09 11:47:34 +0300

    add movie fetching with TMDB API

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Welcome to your Expo app 👋
+TMDB API key required to work
+
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
 
---
+++ services/api.ts
@@ -0,0 +1,29 @@
+export const TMDB_CONFIG = {
+	BASE_URL: "https://api.themoviedb.org/3",
+	API_KEY: process.env.EXPO_PUBLIC_TMDB_TOKEN,
+	headers: {
+		accept: "application/json",
+		Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_TOKEN}`,
+	},
+};
+
+export const fetchMovies = async (searchQuery: string) => {
+	const endpoint = searchQuery
+		? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(
+				searchQuery
+		  )}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+		: `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
+
+	const response = await fetch(endpoint, {
+		method: "GET",
+		headers: TMDB_CONFIG.headers,
+	});
+
+	if (!response.ok) {
+		//@ts-ignore
+		throw new Error("failed to fetch movies", response.statusText);
+	}
+
+	const data = await response.json();
+	return data.results;
+};
